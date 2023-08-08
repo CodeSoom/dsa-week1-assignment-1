0bbd5d7da911a8b916a60208281569c73eb455a4
jihyun-jeon <jhplus13@naver.com>
2023-08-09 00:51:41 +0900

    problem1 완료

--- problem-1/problem-1.test.js
+++ problem-1/problem-1.test.js
@@ -1,18 +1,54 @@
+// 1. 가장 익숙한 방법
+// const solution = (numbers) => {
+// return numbers.reduce((acc, cur) => acc + cur, 0);
+// }
+
+// 2.재귀함수로
+// const solution = (numbers, cnt = 0, sum = 0) => {
+//   if (cnt === numbers.length) {
+//     return sum;
+//   }
+
+//   return sum + solution(numbers, cnt + 1, numbers[cnt]);
+// };
+
+// 3.꼬리 재귀 함수로
+// const solution = (numbers, cnt = 0, sum = 0) => {
+//   if (cnt === numbers.length) {
+//     return sum;
+//   }
+
+//   return solution(numbers, cnt + 1, numbers[cnt] + sum);
+// };
+
+// 4. 꼬리 재귀 최적화
 const solution = (numbers) => {
+  let cnt = 0;
+  let sum = 0;
+
+  while (true) {
+    if (cnt === numbers.length) {
+      return sum;
+    }
+
+    sum += numbers[cnt];
+    cnt += 1;
+  }
 };
 
-test('빈 배열은 0을 반환한다', () => {
+test("빈 배열은 0을 반환한다", () => {
   expect(solution([])).toBe(0);
 });
 
-test('배열의 합을 반환한다', () => {
+test("배열의 합을 반환한다", () => {
   expect(solution([1, 2, 3, 4])).toBe(10);
   expect(solution([-1, 3, 8, 9, 10, 11])).toBe(40);
 });
 
-test('큰 배열이 입력으로 주어져도 RangeError를 던지지 않는다', () => {
+test("큰 배열이 입력으로 주어져도 RangeError를 던지지 않는다", () => {
   const input = Array.from({ length: 10000 }, (_, i) => i + 1);
 
-  expect(() => solution(input))
-    .not.toThrowError(new RangeError('Maximum call stack size exceeded'));
+  expect(() => solution(input)).not.toThrowError(
+    new RangeError("Maximum call stack size exceeded")
+  );
 });
--- problem-2/problem-2.test.js
+++ problem-2/problem-2.test.js
@@ -1,16 +1,34 @@
-const solution = (n) => {
+// 1. 가장 익숙한 방법
+const solution = (n, cnt = 1, a = 0, b = 1) => {
+  if (n === 0) {
+    return 0;
+  }
+
+  if (n === 1) {
+    return 1;
+  }
+  if (cnt === n) {
+    return b;
+  }
+
+  return solution(n, cnt + 1, b, a + b);
 };
 
-test('음수가 주어지면 0을 반환한다', () => {
+// 2. 재귀 함수로
+const solution = (n, cnt = 1, a = 0, b = 1) => {
+  //
+};
+
+test("음수가 주어지면 0을 반환한다", () => {
   expect(solution(-1)).toBe(0);
 });
 
-test('0부터 1까지는 정해진 수를 반환한다', () => {
+test("0부터 1까지는 정해진 수를 반환한다", () => {
   expect(solution(0)).toBe(0);
   expect(solution(1)).toBe(1);
 });
 
-test('2이상 주어지면 앞 두 항의 합을 반환한다', () => {
+test("2이상 주어지면 앞 두 항의 합을 반환한다", () => {
   expect(solution(2)).toBe(1);
   expect(solution(3)).toBe(2);
   expect(solution(4)).toBe(3);
@@ -18,9 +36,10 @@ test('2이상 주어지면 앞 두 항의 합을 반환한다', () => {
   expect(solution(6)).toBe(8);
 });
 
-test('큰 입력이 주어져도 RangeError를 던지지 않는다', () => {
+test("큰 입력이 주어져도 RangeError를 던지지 않는다", () => {
   const input = 100000;
 
-  expect(() => solution(input))
-    .not.toThrowError(new RangeError('Maximum call stack size exceeded'));
+  expect(() => solution(input)).not.toThrowError(
+    new RangeError("Maximum call stack size exceeded")
+  );
 });
