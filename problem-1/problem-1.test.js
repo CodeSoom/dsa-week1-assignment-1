a919cf7d6acaf301186446253414308183dd7a87
hayasha <hayashasong@gmail.com>
2023-08-08 16:16:38 +0900

    Solve dsa-week1-assignment1-problem1
    in 2 possible ways
    - using reduce method
    - using while loop

--- problem-1/problem-1.test.js
+++ problem-1/problem-1.test.js
@@ -1,18 +1,35 @@
-const solution = (numbers) => {
+const solution = (numbers) => (numbers.length ? numbers.reduce((acc, curr) => acc + curr) : 0);
+
+const solution2 = (numbers) => {
+  let acc = 0;
+
+  while (numbers.length > 0) {
+    acc += numbers.pop();
+  }
+
+  return acc;
 };
 
 test('빈 배열은 0을 반환한다', () => {
   expect(solution([])).toBe(0);
+
+  expect(solution2([])).toBe(0);
 });
 
 test('배열의 합을 반환한다', () => {
   expect(solution([1, 2, 3, 4])).toBe(10);
   expect(solution([-1, 3, 8, 9, 10, 11])).toBe(40);
+
+  expect(solution2([1, 2, 3, 4])).toBe(10);
+  expect(solution2([-1, 3, 8, 9, 10, 11])).toBe(40);
 });
 
 test('큰 배열이 입력으로 주어져도 RangeError를 던지지 않는다', () => {
   const input = Array.from({ length: 10000 }, (_, i) => i + 1);
 
   expect(() => solution(input))
+    .not.toThrowError(new RangeError('Maximum call stack size exceeded'));
+
+  expect(() => solution2(input))
     .not.toThrowError(new RangeError('Maximum call stack size exceeded'));
 });
