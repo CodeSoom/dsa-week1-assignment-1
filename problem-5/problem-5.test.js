08cc4bbc9f1799f74dfb73af84c29958e88f4e8e
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-08-09 13:06:31 +0900

    solution: problem-5/solution-1,2

--- problem-5/problem-5.test.js
+++ problem-5/problem-5.test.js
@@ -1,17 +1,23 @@
-const solution = () => {
+//1. 가장 익숙한 방법으로 문제를 해결해 주세요.
+const solution = (b, a) => {
+  if (a === 0) {
+    return b;
+  }
+  return solution(a, b % a);
 };
 
-test('최대 공약수를 반환한다', () => {
+test("최대 공약수를 반환한다", () => {
   expect(solution(4, 12)).toBe(4);
   expect(solution(3, 7)).toBe(1);
   expect(solution(16, 72)).toBe(8);
   expect(solution(9, 12)).toBe(3);
 });
 
-test('큰 입력이 주어져도 RangeError를 던지지 않는다', () => {
+test("큰 입력이 주어져도 RangeError를 던지지 않는다", () => {
   const a = Number.MAX_VALUE;
   const b = 1213;
 
-  expect(() => solution(a, b))
-    .not.toThrowError(new RangeError('Maximum call stack size exceeded'));
+  expect(() => solution(a, b)).not.toThrowError(
+    new RangeError("Maximum call stack size exceeded")
+  );
 });
