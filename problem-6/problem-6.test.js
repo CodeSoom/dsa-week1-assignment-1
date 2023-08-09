a0c053477b3653994c5ef88d54150b36914d7e37
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-08-09 14:39:13 +0900

    solution: problem-6/solution-2

--- problem-6/problem-6.test.js
+++ problem-6/problem-6.test.js
@@ -1,5 +1,6 @@
 //1. 재귀 함수로 문제를 해결해 주세요.
-const solution = (n) => {
+{
+  /*const solution = (n) => {
   if (n === 1) {
     return 1;
   }
@@ -13,6 +14,29 @@ const solution = (n) => {
   }
 
   return solution(n - 1) + solution(n - 2) + solution(n - 3);
+};*/
+}
+
+//2. 다이나믹 프로그래밍으로 최적화 해주세요.
+const solution = (n, memo = []) => {
+  if (n === 1) {
+    return 1;
+  }
+
+  if (n === 2) {
+    return 2;
+  }
+
+  if (n === 3) {
+    return 4;
+  }
+
+  if (!memo[n]) {
+    memo[n] =
+      solution(n - 1, memo) + solution(n - 2, memo) + solution(n - 3, memo);
+  }
+
+  return memo[n];
 };
 
 test("계단에 오를 수 있는 가지 수를 반환한다", () => {
