f0be6f7f3e03a24f2ff1cef13d76f9e727e00cfa
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-08-09 14:16:07 +0900

    solution: problem-6/solution-1

--- problem-6/problem-6.test.js
+++ problem-6/problem-6.test.js
@@ -1,7 +1,21 @@
+//1. 재귀 함수로 문제를 해결해 주세요.
 const solution = (n) => {
+  if (n === 1) {
+    return 1;
+  }
+
+  if (n === 2) {
+    return 2;
+  }
+
+  if (n === 3) {
+    return 4;
+  }
+
+  return solution(n - 1) + solution(n - 2) + solution(n - 3);
 };
 
-test('계단에 오를 수 있는 가지 수를 반환한다', () => {
+test("계단에 오를 수 있는 가지 수를 반환한다", () => {
   expect(solution(1)).toBe(1);
   expect(solution(2)).toBe(2);
   expect(solution(3)).toBe(4);
@@ -14,6 +28,6 @@ test('계단에 오를 수 있는 가지 수를 반환한다', () => {
   expect(solution(10)).toBe(274);
 });
 
-test('큰 입력이 주어져도 시간안에 실행된다', async () => {
+test("큰 입력이 주어져도 시간안에 실행된다", async () => {
   expect(solution(40)).toBe(23837527729);
 });
