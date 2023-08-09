92e324a1b99934a9918782cb13c2fca08827579e
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-08-09 13:06:50 +0900

    solution: problem-5/solution-3,4

--- problem-5/problem-5.test.js
+++ problem-5/problem-5.test.js
@@ -1,9 +1,29 @@
 //1. 가장 익숙한 방법으로 문제를 해결해 주세요.
+//2. 이번에는 재귀 함수로 문제를 해결해 주세요.
+{
+  /*
 const solution = (b, a) => {
   if (a === 0) {
     return b;
   }
   return solution(a, b % a);
+};
+*/
+}
+
+//3. 꼬리 재귀 함수로 바꿔보세요.
+//4. 꼬리 재귀 최적화를 통해서 최적화해 보세요.
+const solution = (b, a) => {
+  let temp;
+  while (true) {
+    if (a === 0) {
+      return b;
+    }
+    temp = a;
+    a = b % temp;
+    b = temp;
+    console.log(b + " " + a);
+  }
 };
 
 test("최대 공약수를 반환한다", () => {
