d7b360dec0b0a8aab3a64641adfd6276a1605aa1
Lee Byoung Seong <dev.bslee@gmail.com>
2023-08-13 00:21:26 +0900

    feat : problem_2 재귀함수

--- problem-1/problem-1.test.js
+++ problem-1/problem-1.test.js
@@ -1,7 +1,9 @@
 const EMPTY_ARRAY_LENGTH = 0;
+const SHOULD_CHUNK_SIZE = 20;
 const INITIALIZE_NUMBER = 0;
 
 /*
+
 1. 가장 익숙한 방법으로 문제를 해결해 주세요.
 const solution = (numbers) => {
   if (numbers.length === EMPTY_ARRAY_LENGTH) {
@@ -11,9 +13,43 @@ const solution = (numbers) => {
 };
 */
 
+/*
+2. 이번에는 재귀 함수로 문제를 해결해 주세요
 const solution = (numbers) => {
+  if (numbers.length === EMPTY_ARRAY_LENGTH) {
+    return EMPTY_ARRAY_LENGTH;
+  }
 
+  if (numbers.length <= SHOULD_CHUNK_SIZE) {
+    // eslint-disable-next-line no-use-before-define
+    return recursion(numbers);
+  }
+
+  // eslint-disable-next-line no-use-before-define
+  const chunkNumbers = chunk(numbers, 10);
+
+  // eslint-disable-next-line no-use-before-define
+  return chunkNumbers.reduce((acc, current) => acc + recursion(current), INITIALIZE_NUMBER);
 };
+
+const recursion = (numbers) => {
+  if (numbers.length === EMPTY_ARRAY_LENGTH) {
+    return EMPTY_ARRAY_LENGTH;
+  }
+
+  return numbers[0] + solution(numbers.slice(1));
+};
+
+const chunk = (data = [], size =1) => {
+  const arr = [];
+
+  for (let i = 0; i < data.length; i += size) {
+    arr.push(data.slice(i, i + size));
+  }
+
+  return arr;
+};
+ */
 
 test('빈 배열은 0을 반환한다', () => {
   expect(solution([])).toBe(0);
