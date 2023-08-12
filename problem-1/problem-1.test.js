a19b2ef4a707146ffc415eae033dd23d4d8f7721
Lee Byoung Seong <dev.bslee@gmail.com>
2023-08-12 23:31:29 +0900

    feat : problem_1 가장 익숙한 방법으로 문제를 해경

--- problem-1/problem-1.test.js
+++ problem-1/problem-1.test.js
@@ -1,3 +1,16 @@
+const EMPTY_ARRAY_LENGTH = 0;
+const INITIALIZE_NUMBER = 0;
+
+/*
+1. 가장 익숙한 방법으로 문제를 해결해 주세요.
+const solution = (numbers) => {
+  if (numbers.length === EMPTY_ARRAY_LENGTH) {
+    return EMPTY_ARRAY_LENGTH;
+  }
+  return numbers.reduce((acc, current) => acc + current, INITIALIZE_NUMBER);
+};
+*/
+
 const solution = (numbers) => {
 
 };
@@ -12,7 +25,7 @@ test('배열의 합을 반환한다', () => {
 });
 
 test('큰 배열이 입력으로 주어져도 RangeError를 던지지 않는다', () => {
-  const input = Array.from({length: 10000}, (_, i) => i + 1);
+  const input = Array.from({ length: 10000 }, (_, i) => i + 1);
 
   expect(() => solution(input))
     .not.toThrowError(new RangeError('Maximum call stack size exceeded'));
