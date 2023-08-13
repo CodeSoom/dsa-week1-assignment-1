c579a77d96cb74049594e48fcfc6c37d5b410df1
Lee Byoung Seong <dev.bslee@gmail.com>
2023-08-13 11:20:17 +0900

    feat : problem_4 가장 익숙한방법 & 꼬리재귀

--- problem-5/problem-5.test.js
+++ problem-5/problem-5.test.js
@@ -1,5 +1,25 @@
-const solution = () => {
+/*
+1. 가장 익숙한 방법
+const solution = (a = 0, b = 0) => {
+  let result = 0;
+  while ((a % b) > 0) {
+    result = a % b;
+    // eslint-disable-next-line no-param-reassign
+    a = b;
+    // eslint-disable-next-line no-param-reassign
+    b = result;
+  }
+  return b;
 };
+ */
+
+// 2. 재귀 => 모르겠음
+
+/*
+3. 꼬리 재귀 함수로 바꿔보세요.
+4. 꼬리 재귀 최적화를 통해서 최적화해 보세요.
+ */
+const solution = (a = 0, b = 0) => (b ? solution(b, a % b) : Math.abs(a));
 
 test('최대 공약수를 반환한다', () => {
   expect(solution(4, 12)).toBe(4);
