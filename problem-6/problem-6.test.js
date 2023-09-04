407ca25c1879d2be55888860588a82403d842c3f
hannut91 <hannut91@gmail.com>
2023-09-04 11:19:58 +0900

    Revert "Merge pull request #2 from YeongseoYoon/main"
    This reverts commit e5ccb85fd32a24deee8ab60543245872f3e1acb2, reversing
    changes made to 4349bc12cdd323bdde61309ea563e95052926dcb.

--- problem-6/problem-6.test.js
+++ problem-6/problem-6.test.js
@@ -1,45 +1,7 @@
-//1. 재귀 함수로 문제를 해결해 주세요.
-{
-  /*const solution = (n) => {
-  if (n === 1) {
-    return 1;
-  }
-
-  if (n === 2) {
-    return 2;
-  }
-
-  if (n === 3) {
-    return 4;
-  }
-
-  return solution(n - 1) + solution(n - 2) + solution(n - 3);
-};*/
-}
-
-//2. 다이나믹 프로그래밍으로 최적화 해주세요.
-const solution = (n, memo = []) => {
-  if (n === 1) {
-    return 1;
-  }
-
-  if (n === 2) {
-    return 2;
-  }
-
-  if (n === 3) {
-    return 4;
-  }
-
-  if (!memo[n]) {
-    memo[n] =
-      solution(n - 1, memo) + solution(n - 2, memo) + solution(n - 3, memo);
-  }
-
-  return memo[n];
+const solution = (n) => {
 };
 
-test("계단에 오를 수 있는 가지 수를 반환한다", () => {
+test('계단에 오를 수 있는 가지 수를 반환한다', () => {
   expect(solution(1)).toBe(1);
   expect(solution(2)).toBe(2);
   expect(solution(3)).toBe(4);
@@ -52,6 +14,6 @@ test("계단에 오를 수 있는 가지 수를 반환한다", () => {
   expect(solution(10)).toBe(274);
 });
 
-test("큰 입력이 주어져도 시간안에 실행된다", async () => {
+test('큰 입력이 주어져도 시간안에 실행된다', async () => {
   expect(solution(40)).toBe(23837527729);
 });
