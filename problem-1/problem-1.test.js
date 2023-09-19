32e07f55609292f36e3feddae46129fe986e5e5b
jinvicky <jinvicky@intube.kr>
2023-09-19 09:41:32 +0900

    Feat: 정답 추가

--- problem-1/problem-1.test.js
+++ problem-1/problem-1.test.js
@@ -4,8 +4,9 @@
 /* eslint-disable consistent-return */
 /* eslint-disable spaced-comment */
 /* eslint-disable no-unused-vars */
+
+//편한 방법
 const solution1 = (numbers) => {
-  //편한 방법
   if (numbers.length === 0) return 0;
   let sum = 0;
   for (let i = 0; i < numbers.length; i++) {
@@ -13,23 +14,54 @@ const solution1 = (numbers) => {
   }
   return sum;
 };
-
-const solution = (numbers) => {
-  // 재귀 함수
+// 재귀 함수 (jin)
+const solution2 = (numbers) => {
   if (numbers.length === 0) return 0;
   const first = numbers[0];
   const sliceArr = numbers.slice(1);
-  return first + solution(sliceArr); //콜 스택 사이즈를 벗어남.
+  return first + solution2(sliceArr); //콜 스택 사이즈를 벗어남.
 };
 
+// 코드숨 문제 풀이 과정
+// 1. 베이스 케이스를 찾기
+// 2. 베이스 케이스 이전의 단계를 찾기 
+// 3. 베이스 케이스 이전의 이전의 단계를 찾기 
+// 4. 거꾸로 문제를 해결해 나가기
+
+// 재귀 함수 정답 (codesoom), 그러나 큰 배열이 입력으로 주어지면 콜스택 사이즈 부족 에러가 발생한다.
 const solution3 = (numbers) => {
-  // 꼬리 재귀함수
   if (numbers.length === 0) return 0;
+
+  const [first] = numbers;
+  return first + solution3(numbers.slice(1));
 };
 
-const solution4 = (numbers) => {
-  // 꼬리 재귀함수 최적화
+// 꼬리재귀최적화 , 그러나 js는 kotlin(tailrec 쓰면 가능)과 달리 꼬리재귀최적화를 지원하지 않음.
+const solution4 = (numbers, sum = 0) => {
+  if (numbers.length === 0) {
+    return sum;
+  }
+  const [first] = numbers;
+  return solution4(numbers.slice(1), sum + first);
 };
+
+// 꼬리재귀최적화를 반복문으로 변경 #함수를 리턴하지 않고 할당한다
+const solution = (n) => {
+  let numbers = [...n]; //인자를 그대로 사용하지 않고 변수에 ...으로 풀어서 사용한다.
+  let sum = 0;
+
+  while(true) {
+    if (numbers.length === 0) { //numbers가 없으면 break;
+      return sum;
+    }
+
+    const [first] = numbers;
+    sum = sum + first;
+    numbers = numbers.slice(1);
+  }
+};
+
+
 
 test("빈 배열은 0을 반환한다", () => {
   expect(solution([])).toBe(0);
