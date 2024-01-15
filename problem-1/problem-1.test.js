8c7daa8e7a27ff37de3720024d3189997df947b7
whoodongpyo <vvhooping@gmail.com>
2024-01-15 15:04:41 +0900

    refactor: 총합 구하기 - 배열을 직접 수정하는 대신 index 사용하기 - ESLint 경고 해결 : while(true) 와 같은 상수 조건을 사용하는 것을 방지

--- problem-1/problem-1.test.js
+++ problem-1/problem-1.test.js
@@ -1,5 +1,5 @@
 // 1. 가장 익숙한 방법으로 문제를 해결해 주세요.
-//const solution = (numbers) => {
+// const solution = (numbers) => {
 //  let answer = 0;
 //
 //  for (let i = 0; i < numbers.length; i += 1) {
@@ -7,10 +7,10 @@
 //  }
 //
 //  return answer;
-//};
+// };
 
 // 2. 이번에는 재귀 함수로 문제를 해결해 주세요.
-//const solution = (numbers) => {
+// const solution = (numbers) => {
 //  // Base Case
 //  if (numbers.length === 0) {
 //    return 0;
@@ -18,34 +18,33 @@
 //
 //  // Recursive Case
 //  return numbers.pop() + solution(numbers);
-//};
+// };
 
 // 3. 꼬리 재귀 함수로 바꿔보세요.
 // -> Maximum call stack size exceeded 발생..
-//const solution = (numbers, acc = 0) => {
+// const solution = (numbers, acc = 0) => {
 //  if (numbers.length === 0) {
 //    return acc;
 //  }
 //
 //  // return solution(numbers.slice(0, numbers.length - 1), acc + numbers[numbers.length - 1]);
 //  return solution(numbers, acc + numbers.pop());
-//};
+// };
 
 // 4. 꼬리 재귀 최적화를 통해서 최적화해 보세요.
 const solution = (numbers) => {
   // 1) 계속해서 바뀌는 값을 변수로 선언한다.
-  const current = numbers;
   let acc = 0;
+  let index = 0;
 
   // 3) 반복문을 만든다.
-  while (true) {
-    if (current.length === 0) {
-      return acc;
-    }
-
+  while (index < numbers.length) {
     // 2) 재귀 함수 호출로 값을 변경하는 대신에 변수의 값을 할당해서 변경한다.
-    acc = acc + current.pop();
+    acc = acc + numbers[index];
+    index += 1;
   }
+
+  return acc;
 };
 
 test('빈 배열은 0을 반환한다', () => {
